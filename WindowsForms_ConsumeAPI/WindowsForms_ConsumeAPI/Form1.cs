fa49e3d33ae704266e77b91c0984f08e825d52b8
dvarelaminstacredit <dvarelam@instacredit.com>
2023-06-29 10:35:38 -0600

    Test de autenticacion

--- WindowsForms_ConsumeAPI/WindowsForms_ConsumeAPI/Form1.cs
+++ WindowsForms_ConsumeAPI/WindowsForms_ConsumeAPI/Form1.cs
@@ -39,12 +39,12 @@ namespace WindowsForms_ConsumeAPI
         {
             var cliente = new HttpClient();
 
-            Credenciales oUsuaario = new Credenciales() { key = "h@gmail.com", secret = "123" };
+            Credenciales oUsuaario = new Credenciales() { key = "LKCQUXEZQV", secret = "KFQCXKNQPHWCEVZMJKAX" };
 
              //se serializa el objeto Usuario                    //contenido, encoding, tipo 
             var content = new StringContent(JsonConvert.SerializeObject(oUsuaario), Encoding.UTF8, "application/json");
             //aquí se  ejecuta la API
-            var response = await cliente.PostAsync("http://localhost:5252/api/Autenticacion/Validar", content);
+            var response = await cliente.PostAsync("https://apiv5.dev-ridivi.com/v5/auth/token", content);
             //SE OBTIENE LA RESPUESTA
             var json_respuesta = await response.Content.ReadAsStringAsync();
             //SE DESEREALIZA LA RESPUESTA Y SE CONVIERTE EN OBJETO
@@ -54,7 +54,9 @@ namespace WindowsForms_ConsumeAPI
             //se la pasa el token al segundo cliente que va a consumir algún método del API
             cliente2.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", orespuesta.token);
 
-            var response2 = await cliente2.GetAsync("http://localhost:5252/api/Producto/Lista");
+            var content2 = new StringContent(JsonConvert.SerializeObject(orespuesta), Encoding.UTF8, "application/json");
+
+            var response2 = await cliente2.PostAsync("https://apiv5.dev-ridivi.com/v5/auth/checkToken", content2);
 
             var json_respuesta2 = await response2.Content.ReadAsStringAsync();
 
